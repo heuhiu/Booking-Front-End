ff68601853bfc67d501680cd1fe358f0e6ad19c2
heuhiu <hieuvuong3562@gmail.com>
2020-06-08 22:32:35 +0700

    [Modified]

--- src/App.js
+++ src/App.js
@@ -4,50 +4,74 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="#">Start Bootstrap</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="/#">Start Bootstrap</a>
+          <i class="fas fa-user"></i>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/#">Home
+                <span className="sr-only">(current)</span>
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+              <li className="nav-item">
+                <a className="nav-link" href="///#">About</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Services</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Contact</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <header class="masthead">
-        <div class="container h-100">
-          <div class="row h-100 align-items-center">
-            <div class="col-12 text-center">
-              <h1 class="font-weight-light">Vertically Centered Masthead Content</h1>
-              <p class="lead">A great starter layout for a landing page</p>
+      <header className="masthead">
+        <div className="container h-100">
+          <div className="row h-100 align-items-center">
+            <div className="col-12 text-center">
+              <h1 className="font-weight-light">Vertically Centered Masthead Content</h1>
+              <p className="lead">A great starter layout for a landing page</p>
+              <div className="block-17 my-4">
+                <form action="" method="post" className="d-block d-flex">
+                  <div className="fields d-block d-flex">
+                    <div className="textfield-search one-third">
+                      <input type="text" className="form-control" placeholder="Ex: food, service, hotel" />
+                    </div>
+                    <div className="select-wrap one-third">
+                      <div className="icon"><span className="fas fa-arrow-down"></span></div>
+                      <select name="" id="" className="form-control" placeholder="Keyword search">
+                        <option value="">Where</option>
+                        <option value="">San Francisco USA</option>
+                        <option value="">Berlin Germany</option>
+                        <option value="">Lodon United Kingdom</option>
+                        <option value="">Paris Italy</option>
+                      </select>
+                    </div>
+                  </div>
+                  <input type="submit" className="search-submit btn btn-primary" value="Search" />
+                </form>
+              </div>
+
             </div>
           </div>
         </div>
       </header>
 
-      <section class="py-5">
-        <div class="container">
-          <h2 class="font-weight-light">Page Content</h2>
+      <section className="py-5">
+        <div className="container">
+          <h2 className="font-weight-light">Page Content</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus ab nulla dolorum autem nisi officiis blanditiis voluptatem hic, assumenda aspernatur facere ipsam nemo ratione cumque magnam enim fugiat reprehenderit expedita.</p>
         </div>
       </section>
+
+
     </div>
   );
 }
