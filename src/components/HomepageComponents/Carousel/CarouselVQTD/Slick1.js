2cf8129a2d6e6c53a04fd39bb5a7f28bdf1816d6
heuhiu <hieuvuong3562@gmail.com>
2020-06-12 14:54:06 +0700

    [Css change]

--- src/components/HomepageComponents/Carousel/CarouselVQTD/Slick1.js
+++ src/components/HomepageComponents/Carousel/CarouselVQTD/Slick1.js
@@ -53,7 +53,7 @@ class Slick1 extends Component {
 
   render() {
     const settings = {
-      dots: true,
+      // dots: true,
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
