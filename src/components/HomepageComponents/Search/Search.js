0c1b8a5f15b89a3738a12456bd4ed8cfcf6b13f6
heuhiu <hieuvuong3562@gmail.com>
2020-06-26 01:08:37 +0700

    [SeachBox drop]

--- src/components/HomepageComponents/Search/Search.js
+++ src/components/HomepageComponents/Search/Search.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import './style.css';
 import LogoSearch from '../../../img/LogoSearch.png';
 import search from '../../../img/search.png';
+import { Button, Collapse } from 'react-bootstrap';
 
 class Search extends Component {
 
@@ -13,39 +14,51 @@ class Search extends Component {
     }
     render() {
         return (
-            <form action="" method="post" className="d-block d-flex">
-                <div className="fields d-block d-flex">
-                    <div className="textfield-search two-third">
-                        <img src={LogoSearch} alt="?" width="54.467" height="43.84" />
-                    </div>
-                    <div className="textfield-search one-third">
-                        <input type="text" className="form-control" 
-                        placeholder="Ex: food, service, hotel" />
-                    </div>
-                    <div className="select-wrap one-third">
-                        <div className="icon">
-                            <span className="fas fa-arrow-down"></span>
+            <div>
+                <form action="" method="post" className="d-block d-flex">
+                    <div className="fields d-block d-flex">
+                        <div className="textfield-search two-third">
+                            <img src={LogoSearch} alt="?" width="54.467" height="43.84" />
                         </div>
-                        <select name="" id="" className="form-control"
+                        <div className="textfield-search one-third">
+                            <input type="text" className="form-control"
+                                placeholder="Ex: food, service, hotel" />
+                        </div>
+                        <div className="select-wrap one-third">
+                            {/* <div className="icon">
+                            <span className="fas fa-arrow-down"></span>
+                        </div> */}
+                            {/* <select name="" id="" className="form-control"
                             placeholder="Keyword search">
                             <option value="">Bộ Lọc</option>
                             <option value="">San Francisco USA</option>
                             <option value="">Berlin Germany</option>
                             <option value="">Lodon United Kingdom</option>
                             <option value="">Paris Italy</option>
-                        </select>
+                        </select> */}
+                            <div className="form-control">
+                                <span>Bộ lọc</span>
+
+                            </div>
+
+                        </div>
+
                     </div>
-                </div>
-                <button type="submit"
-                    className="search-submit btn btn-primary ">
-                    {/* <i class="fas fa-search"></i>&nbsp;Search */}
-                    <img src={search} 
-                    alt ="Fail !" 
-                    width="17.76" 
-                    height="17.76"
-                    /> &nbsp; Tìm Kiếm
+                    <button type="submit"
+                        className="search-submit btn btn-primary ">
+                        {/* <i class="fas fa-search"></i>&nbsp;Search */}
+                        <img src={search}
+                            alt="Fail !"
+                            width="17.76"
+                            height="17.76"
+                        /> &nbsp; Tìm Kiếm
                 </button>
-            </form>
+                    <div className="filterBox">
+
+                    </div>
+                </form>
+
+            </div>
         );
     }
 
